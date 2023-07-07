6da9276c26ad35e04882acf8e08147520fc13e05
macsta31 <mackstathis@gmail.com>
2023-07-06 22:00:58 -0400

    migration to mailgun for email

--- src/routes/email/+server.ts
+++ src/routes/email/+server.ts
@@ -2,26 +2,21 @@ import { env } from '$env/dynamic/private';
 // import Hello from '$lib/emails/EmailResults.svelte';
 import nodemailer from 'nodemailer';
 import { json } from '@sveltejs/kit';
-import { VITE_EMAIL, VITE_PW } from '$env/static/private';
+import { VITE_APIKEY, VITE_DOMAIN } from '$env/static/private';
+
+const API_KEY = VITE_APIKEY;
+const DOMAIN = VITE_DOMAIN;
+
+import formData from 'form-data';
+import Mailgun from 'mailgun.js';
 
 
-export async function POST({ request }) {
-  const { searchParam, scanResult } = await request.json()
-  // console.log(VITE_EMAIL, VITE_PW)
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: env.VITE_EMAIL,
-      pass: env.VITE_PW
-    },
-    port: 587, // Use port 587 for secure SMTP
-    secure: false, // Use STARTTLS to upgrade the connection to secure
-});
+async function sendMail(searchParam: any, scanResult: { Name: any; Title: any; Domain: any; BreachDate: any; PwnCount: any; Description: any; DataClasses: any[]; IsVerified: any; }[]) {
+  const mailgun = new Mailgun(formData);
+  const client = mailgun.client({username: 'api', key: API_KEY});
 
-
-  // console.log(searchParam, scanResult)
-  const mailOptions = {
-    from: 'mackstathis@gmail.com',
+  const messageData = {
+    from: 'Excited User <me@samples.mailgun.org>',
     to: searchParam,
     subject: `Dark Web Scan Results For ${searchParam}`,
     text: scanResult.map((result: { Name: any; Title: any; Domain: any; BreachDate: any; PwnCount: any; Description: any; DataClasses: any[]; IsVerified: any; }) => {
@@ -29,12 +24,25 @@ export async function POST({ request }) {
     }).join('\n')
   };
 
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
-  return json('response')
+  return client.messages.create(DOMAIN, messageData)
+    .then((res) => {
+      console.log(res);
+      return { 
+        status: 200, 
+        body: res
+      };
+    })
+    .catch((err) => {
+      console.error(err);
+      return { 
+        status: 500, 
+        body: err.message
+      };
+    });
+}
+
+export async function POST({ request }) {
+  const { searchParam, scanResult } = await request.json();
+  const res = await sendMail(searchParam, scanResult);
+  return json(res)
 }
