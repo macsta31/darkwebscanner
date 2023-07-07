c1edf063f96efc81a351e0c9abe179c0548085de
macsta31 <mackstathis@gmail.com>
2023-07-06 20:59:51 -0400

    modified port for nodemailer

--- src/routes/email/+server.ts
+++ src/routes/email/+server.ts
@@ -2,16 +2,20 @@ import { env } from '$env/dynamic/private';
 // import Hello from '$lib/emails/EmailResults.svelte';
 import nodemailer from 'nodemailer';
 import { json } from '@sveltejs/kit';
+import { VITE_EMAIL, VITE_PW } from '$env/static/private';
 
 
 export async function POST({ request }) {
   const { searchParam, scanResult } = await request.json()
+  // console.log(VITE_EMAIL, VITE_PW)
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: env.VITE_EMAIL,
       pass: env.VITE_PW
-    }
+    },
+    port: 465, // Use port 465 for secure SMTP
+    secure: true
   });
 
   // console.log(searchParam, scanResult)
